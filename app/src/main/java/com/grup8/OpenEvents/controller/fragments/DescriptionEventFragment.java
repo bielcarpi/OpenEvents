02f5aae91f45fcca10e121b1dfcc761b16faf4a8
david.deu <david.deu@students.salle.url.edu>
2022-05-09 10:00:53 +0200

    No se la verdad

---
+++ app/src/main/java/com/grup8/OpenEvents/controller/fragments/DescriptionEventFragment.java
@@ -0,0 +1,28 @@
+package com.grup8.OpenEvents.controller.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.grup8.OpenEvents.R;
+
+public class DescriptionEventFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description_event, container, false);
+    }
+}
